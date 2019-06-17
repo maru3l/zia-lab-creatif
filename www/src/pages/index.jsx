89aff1d025cdb70d10c237def000a33035a3d2cb
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-06-17 11:39:17 -0400

    add all views to index page

--- www/src/pages/index.jsx
+++ www/src/pages/index.jsx
@@ -1,215 +1,59 @@
 // vendors
 import React from "react"
+import Image from "gatsby-image"
+import { graphql } from "gatsby"
 
 // components
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+// views
+import Home from "../views/Home"
+import Mission from "../views/Mission"
+import Mandats from "../views/Mandats"
+import Board from "../views/Board"
+import FooterView from "../views/footer"
+import Hero from "../views/Hero"
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut culpa
-      mollitia debitis neque error sunt aperiam voluptatem magni, eligendi
-      dolore rem est facere, nobis, itaque ea sed molestiae quo!
-    </p>
+    <Hero />
 
-    <h1>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, amet!
-    </h1>
+    <Home />
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nihil
-      beatae, nemo quidem hic sit quo mollitia error saepe. Repellat cumque
-      magni unde praesentium a, amet aspernatur odit doloribus nobis!
-    </p>
+    <Mission />
 
-    <h2>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore,
-      accusamus!
-    </h2>
+    <Image fluid={data.bgMandats.childImageSharp.fluid} />
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nesciunt
-      voluptas doloremque delectus ducimus rerum cumque assumenda. Ex, amet
-      ratione explicabo at architecto ab adipisci! Amet inventore temporibus
-      iure eaque?
-    </p>
+    <Mandats />
 
-    <h3>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, earum!
-    </h3>
+    <Image fluid={data.bgOrganisation.childImageSharp.fluid} />
 
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro architecto
-      aliquid perferendis ullam, fugiat harum commodi corporis tempore eum quae
-      nobis fugit totam amet nam repudiandae vero repellendus, accusamus
-      deserunt?
-    </p>
+    <Board />
 
-    <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, optio.</h4>
-
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
-      similique libero fugiat placeat tenetur distinctio voluptas consequatur
-      facere? Iure alias odio dignissimos tempora, error architecto quam ab
-      facere voluptate nesciunt?
-    </p>
-
-    <h5>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, facere!
-    </h5>
-
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-      cumque, mollitia fuga aliquid unde ex enim repudiandae commodi recusandae,
-      omnis delectus voluptas libero, animi minus. Sit itaque reprehenderit
-      ullam praesentium?
-    </p>
-
-    <h6>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, fugiat!
-    </h6>
-
-    <ul>
-      <li>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolore
-        nobis molestias! A expedita hic accusantium beatae, distinctio quis
-        deleniti excepturi amet maiores provident labore? Assumenda nulla
-        architecto temporibus non?
-      </li>
-      <li>
-        Non unde nobis optio magni at eum culpa blanditiis quam pariatur eos
-        provident rem, odio sint nisi deserunt. Cupiditate, veritatis autem?
-        Officia libero consequatur hic ex perspiciatis temporibus repudiandae!
-        Laudantium?
-      </li>
-      <li>
-        Ut corporis harum impedit sint excepturi laborum voluptate minima quas?
-        Consectetur nesciunt animi neque facilis voluptatem accusamus adipisci,
-        esse nisi similique sit consequuntur fuga repellat amet expedita
-        doloremque quam est!
-      </li>
-      <li>
-        Repellendus quae quo sit facere iure ipsam, ab libero provident dolores!
-        Dolor dolore laboriosam perspiciatis vel atque, suscipit voluptates,
-        quia consectetur natus dicta et optio eligendi commodi temporibus minima
-        amet?
-      </li>
-      <li>
-        Laudantium, quisquam animi. Placeat deserunt, aliquid doloribus harum
-        suscipit esse quis, modi dolores, dolor facere assumenda quasi delectus
-        maiores reiciendis? Dolor est laboriosam ipsam nam ad nesciunt
-        voluptatem vel molestias.
-      </li>
-      <li>
-        Dolorum hic totam aliquam neque. Adipisci corrupti aliquam magni
-        asperiores error consectetur voluptatum quaerat. Amet, suscipit. Odit at
-        repudiandae numquam odio nostrum neque. Dolor ducimus recusandae
-        voluptas repellendus similique fugit!
-      </li>
-      <li>
-        Soluta deleniti obcaecati vitae ea unde odio itaque quia, accusantium,
-        cupiditate labore veritatis dolorum! Aliquam magnam fugiat ducimus quia
-        beatae ea, autem ipsam perspiciatis adipisci dolor voluptatibus,
-        reiciendis maxime soluta.
-      </li>
-      <li>
-        Soluta, doloribus enim quidem, adipisci est labore magni eaque cum
-        cumque quos odio perspiciatis laboriosam incidunt aliquid quod, nisi
-        quasi quo ut nostrum libero et voluptates eius! Rem, possimus
-        consectetur.
-      </li>
-      <li>
-        Ex nam placeat asperiores neque! Harum asperiores eveniet sint impedit
-        temporibus quaerat nostrum, tempore eos dicta tenetur blanditiis ut
-        doloremque. Nam officiis distinctio eligendi facilis quas harum, numquam
-        explicabo placeat.
-      </li>
-      <li>
-        Et vero similique cum, id nemo animi nobis! Ut temporibus, quos iusto
-        debitis quia harum corrupti quae ex modi eius, sit nemo deserunt odio
-        quidem facere, dolores aspernatur eaque saepe.
-      </li>
-    </ul>
-
-    <ol>
-      <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat dolor
-        quia dicta, laborum, quam consectetur nihil blanditiis nobis sint,
-        incidunt aliquam cum? Repudiandae non ipsam pariatur, totam dicta dolor
-        deleniti.
-      </li>
-      <li>
-        Voluptatibus eos sapiente, corrupti voluptatum asperiores laboriosam
-        cupiditate sint temporibus officia fugit animi! Eum quos adipisci ipsa
-        inventore veniam quae deserunt vitae eaque? Nemo asperiores
-        reprehenderit fugiat? Fuga, facere modi.
-      </li>
-      <li>
-        Laudantium facilis magnam quod est, non tempore quis accusamus. Sequi,
-        quos rerum maxime, inventore dolor, recusandae aliquid eum dicta qui
-        laudantium amet enim nemo? Dolore animi aperiam error! Delectus, quis.
-      </li>
-      <li>
-        Minima, obcaecati, praesentium nisi doloremque veritatis porro est
-        pariatur debitis tempore mollitia cupiditate cumque libero laboriosam
-        dolore vero! Tempore quo ex consequuntur officia expedita dolore fugit
-        molestias soluta ipsum maxime!
-      </li>
-      <li>
-        Ab aut consectetur temporibus rerum fugiat excepturi inventore veniam
-        ipsa, consequatur voluptate optio architecto libero in velit maiores
-        odit nemo eaque numquam minima, voluptas provident. Tenetur laudantium
-        nemo natus exercitationem.
-      </li>
-      <li>
-        Voluptatibus dignissimos amet iusto neque quos facere? Rerum,
-        consequatur mollitia aspernatur corporis quis necessitatibus inventore a
-        iusto cum. Debitis eveniet officiis suscipit quaerat modi. Iusto
-        possimus deleniti consectetur voluptatem fuga.
-      </li>
-      <li>
-        Officia unde velit doloribus optio quidem libero, nobis cupiditate
-        consequatur quae consectetur ratione commodi atque natus similique
-        laborum voluptates animi reiciendis possimus consequuntur quos. Mollitia
-        atque quod et molestiae accusantium.
-      </li>
-      <li>
-        Labore magnam commodi ex beatae! Maxime recusandae nostrum quasi
-        repudiandae blanditiis earum hic, vitae cum ea veniam, quo quaerat
-        dolore quidem libero officia numquam tempore voluptas! Magni doloribus
-        delectus architecto.
-      </li>
-      <li>
-        Animi quod voluptatibus iusto facilis atque, sunt explicabo dolorem
-        eaque, maxime deserunt rem enim perferendis repellendus accusamus minus
-        cupiditate, provident nobis. Voluptatibus voluptate voluptas est sunt
-        explicabo quae dolorem cum.
-      </li>
-      <li>
-        Voluptas, cum, eveniet tempore soluta porro perspiciatis magnam
-        repellendus et nulla natus iure? Rerum, aspernatur aliquid eos quaerat
-        odit mollitia labore consequatur, cupiditate, delectus voluptatem
-        dignissimos vitae recusandae qui nulla!
-      </li>
-    </ol>
-
-    <ol>
-      <li>Lorem ipsum dolor sit amet.</li>
-      <li>Sed, eum culpa? Dolore, voluptas.</li>
-      <li>Totam consequatur officiis suscipit culpa.</li>
-      <li>Repellendus placeat necessitatibus veniam a.</li>
-      <li>Id atque saepe odit qui.</li>
-    </ol>
-
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti ipsum
-      non sed officiis alias quaerat, obcaecati libero. Nihil tenetur saepe
-      quam? Quasi sunt cumque nam suscipit reprehenderit esse unde voluptates?
-    </p>
+    <FooterView />
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexPage {
+    bgMandats: file(name: { eq: "bg-mandats" }) {
+      childImageSharp {
+        fluid(maxWidth: 2560) {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+    bgOrganisation: file(name: { eq: "bg-organisation" }) {
+      childImageSharp {
+        fluid(maxWidth: 2560) {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+  }
+`
