58b19f48541f8478231fa4e4735c266404ef3f9d
Marc-Antoine Ruel <hello@marcantoineruel.com>
2020-04-09 14:13:47 -0400

    update performance on projet section

---
+++ www/src/views/Project/ProjectTemplate/ProjectTemplate.jsx
@@ -0,0 +1,96 @@
+// vendors
+import React, { useRef, useEffect, useState } from "react"
+import { css } from "@emotion/core"
+import { fluidRange } from "polished"
+
+import { breakpoints } from "../../../styles/variables"
+import Button from "../../../components/Button/Button"
+
+import Wrapper from "./shared/Wrapper"
+import ProjectModal from "./ProjectModal"
+
+const ProjectTemplate = ({ project, onInViewport, colorSet }) => {
+  const intersectionRef = useRef()
+  const [open, setOpen] = useState(false)
+
+  const handleClick = () => setOpen(!open)
+
+  useEffect(() => {
+    const intersectionElement = intersectionRef.current
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) onInViewport()
+        })
+      },
+      {
+        threshold: [0.55],
+      }
+    )
+
+    observer.observe(intersectionElement)
+
+    return () => {
+      observer.disconnect(intersectionElement)
+    }
+  }, [intersectionRef, onInViewport])
+
+  return (
+    <article
+      ref={intersectionRef}
+      css={css`
+        position: relative;
+      `}
+    >
+      <Wrapper
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 1em;
+        `}
+      >
+        <h3
+          css={css`
+            text-align: center;
+            font-size: ${38 / 10}em;
+            letter-spacing: ${59 / 1000}em;
+            margin: 0;
+            transform: rotate(-90deg);
+            ${fluidRange(
+              {
+                prop: "font-size",
+                fromSize: `${31 / 10}rem`,
+                toSize: `${38 / 10}rem`,
+              },
+              "320px",
+              "768px"
+            )};
+
+            ${breakpoints.mediaQueries.ratio11} {
+              max-width: ${300 / 38}em;
+            }
+          `}
+        >
+          <Button
+            onClick={handleClick}
+            color={colorSet.button.color}
+            underlineColor={colorSet.button.underline}
+          >
+            {project.title}
+          </Button>
+        </h3>
+      </Wrapper>
+
+      {open && (
+        <ProjectModal
+          project={project}
+          onClose={handleClick}
+          colorSet={colorSet}
+        />
+      )}
+    </article>
+  )
+}
+
+export default ProjectTemplate
---
+++ www/src/views/Project/ProjectTemplate/shared/Wrapper.jsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled"
+
+export default styled.div`
+  width: 100vh;
+  max-width: 100vw;
+  margin: auto;
+  min-height: 100vh;
+`
